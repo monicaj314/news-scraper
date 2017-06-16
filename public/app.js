60eebfead7957407472c5ffaa18df310775d5be3
Monica Jones <monicaj314@yahoo.com>
2017-06-15 23:52:10 -0500

    rmv reg exp

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link.replace(/'|"/g, "") + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     console.log(data);
   }
 });
